38fbde9cf90dfab1417853ccf1b3f14d641f0551
Mardson Santana <mardsonsantana105@gmail.com>
2022-11-08 18:37:13 -0300

    inicia o projeto

---
+++ src/main/java/br/com/petz/clientepet/ClientePetApplication.java
@@ -0,0 +1,13 @@
+package br.com.petz.clientepet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientePetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientePetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/petz/clientepet/ClientePetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petz.clientepet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientePetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
