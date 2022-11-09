16153cffc4627c202dbc51d8eaa8559bbe0f50fc
Mardson Santana <mardsonsantana105@gmail.com>
2022-11-09 14:56:24 -0300

    configura context-path

--- src/main/java/br/com/petz/clientepet/ClientePetApplication.java
+++ src/main/java/br/com/petz/clientepet/ClientePetApplication.java
@@ -2,9 +2,18 @@ package br.com.petz.clientepet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ClientePetApplication {
+	
+	public String getHomeTest() {
+		return "Cliente pet - API Home";
+		
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientePetApplication.class, args);
