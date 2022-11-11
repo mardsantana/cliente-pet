6d031cbf09e62a7e53d17ae70f33f6b11c3d1866
Mardson Santana <mardsonsantana105@gmail.com>
2022-11-11 15:36:29 -0300

    implementacliente repository, teste ok!

--- src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
@@ -4,16 +4,19 @@ import org.springframework.stereotype.Repository;
 
 import br.com.petz.clientepet.cliente.application.repository.ClienteRepository;
 import br.com.petz.clientepet.cliente.domain.Cliente;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Repository
 @Log4j2
+@RequiredArgsConstructor
 public class ClienteInfraRepository implements ClienteRepository {
-	
+	private final ClienteSpringDataJPARepository clienteSpringDataJPARepository;
 
 	@Override
 	public Cliente salva(Cliente cliente) {
 		log.info("[inicia] ClienteInfraRepository - salva");
+		clienteSpringDataJPARepository.save(cliente);
 		log.info("[finaliza] ClienteInfraRepository - salva");
 		return cliente;
 	}
---
+++ src/main/java/br/com/petz/clientepet/cliente/infra/ClienteSpringDataJPARepository.java
@@ -0,0 +1,11 @@
+package br.com.petz.clientepet.cliente.infra;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.petz.clientepet.cliente.domain.Cliente;
+
+public interface ClienteSpringDataJPARepository extends JpaRepository<Cliente, UUID> {
+
+}
